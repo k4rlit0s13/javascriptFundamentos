b294962703097e27b8bc1088e3ad8ff921961020
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-05-06 13:52:08 -0500

    comentarios

---
+++ 3.comentarios/alert.js
@@ -0,0 +1,25 @@
+// Este comentario ocupa una línea propia.
+alert('Hello');
+
+alert('World'); // Este comentario sigue a la sentencia.
+
+
+/* Un ejemplo con dos mensajes.
+Este es un comentario multilínea.
+*/
+alert('Hola');
+alert('Mundo'); 
+
+
+/* Comentando el código
+alert('Hola');
+*/
+alert('Mundo');
+
+
+//"error"
+
+/*
+  /* comentario anidado ?!? */
+  */
+  alert( 'Mundo' );
