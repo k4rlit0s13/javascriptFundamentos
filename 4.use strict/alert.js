0f0ae970e32482fc049498a8fd489d51d0ac9aa8
k4rlit0s13 <156240698+k4rlit0s13@users.noreply.github.com>
2024-05-07 06:37:20 -0500

    el modo estricto moderno use strict

---
+++ 4.use strict/alert.js
@@ -0,0 +1,12 @@
+'use strict';  //el modo estricto
+
+
+
+(function(){
+    'use strict';
+    //...código...
+})()
+
+//Entonces, por ahora "use strict"; es un invitado 
+// bienvenido al tope de tus scripts. Luego, cuando tu 
+// código sea todo clases y módulos, puedes omitirlo.
